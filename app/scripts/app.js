ba093cf2df2cf52f5b52256225007b091520f331
Paolo Chillari <killer.paolo@gmail.com>
2013-10-23 10:12:22 +0100

    modifications due to the upgrade of angualar to the new version

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,38 @@
 'use strict';
 
-angular.module('potatoFlickrApp', ['potato.Services'])
+angular.module('potatoFlickrApp', ['potato.Services', 'ngRoute', 'ngAnimate', 'ngSanitize'])
     .config(function ($routeProvider) {
     $routeProvider
         .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'feedsController'
+        controller: 'feedsController',
+        resolve: {
+            feeds: function (feedService) {
+                return feedService.getFeeds('');
+            }
+        }
+    })
+        .when('/feedDetail/:feedIndex', {
+        templateUrl: 'views/feedDetail.html',
+        controller: 'FeeddetailCtrl',
+        resolve: {
+            feed: function (feedService, $route) {
+                return feedService.getFeed($route.current.params.feedIndex);
+            }
+        }
     })
         .otherwise({
         redirectTo: '/'
     });
+});
+
+angular.module('potatoFlickrApp').run(function ($rootScope) {
+    $rootScope.$on("$locationChangeSuccess", function (event, next, current) {
+        if (next.match(/\/feedDetail\//)) {
+            $rootScope.animationDirection = 'reverse';
+        } else {
+            $rootScope.animationDirection = '';
+        }
+    });
+
 });
